b693021979314ae0b5cf660d3ea26c62494a2696
DMartinCodes <dennis.martin.1010@outlook.com>
2023-01-20 12:58:14 -0500

    New commit

---
+++ power.js
@@ -0,0 +1,25 @@
+let weather = {
+    apiKey: "3e37c7f566280252cb311273fe092d15",
+
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}" 
+            +lat 
+            + lon 
+            + "&units=metric&appid=" 
+            + part 
+            +this.apiKey
+        ).then((response) => response.json()).then((data) => this.displayWeather(data));
+
+    },
+
+    displayWeather: function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather;
+        const {temp, humidity} = data.main;
+        const {speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    },
+    
+
+};
